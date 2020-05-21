2020efc77f552843f0dcd628af88bd0882928164
Mike Peterson <mike@sparkabilitygroup.com>
2020-05-20 20:24:49 -0700

    - Moved most of the startup logic from Global to Rock.WebStartup. This fixes an issue where things (like HttpModules) that need to run pre-startup would run before any Migrations or Caching was done, which could cause HttpModules to not start, or duplicate attributes to be created.

--- Rock.WebStartup/AssemblyInitializer.cs
+++ Rock.WebStartup/AssemblyInitializer.cs
@@ -14,72 +14,23 @@
 // limitations under the License.
 // </copyright>
 //
-using System;
-using System.Web;
+
+using Rock.WebStartup;
 
 namespace Rock
 {
     /// <summary>
-    /// Initializer for the project and also used by Rock for all startup logic relating to the web project.
+    /// Initializer that runs prior to RockWeb's Global.Application_Start. (see comments on PreApplicationStartMethod in AssemblyInfo.cs)
+    /// This calls <seealso cref="RockApplicationStartupHelper.RunApplicationStartup"/> to take care of most for all startup logic relating to the web project.
     /// </summary>
     public static class AssemblyInitializer
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <seealso cref="System.Exception" />
-        internal class RockAssemblyInitializerException : Exception
-        {
-            public RockAssemblyInitializerException( string message, Exception innerException )
-                : base( message, innerException )
-            {
-            }
-        }
-
         /// <summary>
         /// Initializes this instance.
         /// </summary>
         public static void Initialize()
         {
-            try
-            {
-                var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-
-                if ( System.Web.Hosting.HostingEnvironment.IsDevelopmentEnvironment )
-                {
-                    try
-                    {
-                        System.Diagnostics.Debug.WriteLine( string.Format( "Application_Initialize: {0}", RockDateTime.Now.ToString( "hh:mm:ss.FFF" ) ) );
-                        new Rock.Model.AttributeService( new Data.RockContext() ).GetSelect( 0, a => a.Id );
-                        System.Diagnostics.Debug.WriteLine( string.Format( "ConnectToDatabase - {0} ms", stopwatch.Elapsed.TotalMilliseconds ) );
-                        stopwatch.Restart();
-                    }
-                    catch
-                    {
-                        // ignore
-                    }
-                }
-
-                // Step 1: Load registered HTTP Modules - http://blog.davidebbo.com/2011/02/register-your-http-modules-at-runtime.html
-                var activeHttpModules = Rock.Web.HttpModules.HttpModuleContainer.GetActiveComponents(); // takes 8 seconds :(
-
-                foreach ( var httpModule in activeHttpModules )
-                {
-                    HttpApplication.RegisterModule( httpModule.GetType() );
-                }
-
-                if ( System.Web.Hosting.HostingEnvironment.IsDevelopmentEnvironment )
-                {
-                    System.Diagnostics.Debug.WriteLine( string.Format( "WebStartup.Initialize - {0} ms", stopwatch.Elapsed.TotalMilliseconds ) );
-                }
-            }
-            catch ( Exception ex )
-            {
-                // incase something bad happens when access the database, like a problem with a migration. We will just log this,
-                var rockAssemblyInitializerException = new RockAssemblyInitializerException( $"Exception in AssemblyInitializer.Initialize {ex.Message}. It is OK to ignore this because it is probably due to pending migrations.", ex );
-                System.Diagnostics.Debug.WriteLine( rockAssemblyInitializerException.Message );
-                Rock.Model.ExceptionLogService.LogException( rockAssemblyInitializerException );
-            }
+            RockApplicationStartupHelper.RunApplicationStartup();
         }
     }
 }

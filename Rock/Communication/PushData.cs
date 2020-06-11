74421a7aa4087199161244db85b16daa9961aedd
Matt Bronkema <matt.bronkema@sparkdevnetwork.org>
2020-06-11 10:45:19 -0400

    - Completed PushNotification control updates.

---
+++ Rock/Communication/PushData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock.Communication
+{
+    internal class PushData
+    {
+        public int? MobilePageId { get; set; }
+        public Dictionary<string, string> MobilePageQueryString { get; set; }
+        public int? MobileApplicationId { get; set; }
+        public string Url { get; set; }
+    }
+}
--- Rock/Web/UI/Controls/Communication/PushNotification.cs
+++ Rock/Web/UI/Controls/Communication/PushNotification.cs
@@ -13,9 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // </copyright>
-//
+
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -23,7 +22,6 @@ using System.Web.UI.WebControls;
 using Rock.Communication;
 using Rock.Model;
 using Rock.Utility;
-using Rock.Web.Cache;
 
 namespace Rock.Web.UI.Controls.Communication
 {
@@ -33,12 +31,11 @@ namespace Rock.Web.UI.Controls.Communication
     public class PushNotification : MediumControl
     {
         #region UI Controls
-
         private RockControlWrapper rcwMessage;
         private MergeFieldPicker mfpMessage;
+        private Label lblCount;
         private RockTextBox tbMessage;
         private RockTextBox tbTitle;
-        private RockCheckBox cbSound;
 
         private ImageUploader iupPushImage;
         private RockRadioButtonList rbOpenAction;
@@ -46,7 +43,7 @@ namespace Rock.Web.UI.Controls.Communication
         private RockDropDownList ddlMobileApplications;
         private KeyValueList kvlQuerystring;
         private PagePicker ppMobilePage;
-
+        private UrlLinkBox urlLink;
         #endregion
 
         #region Properties
@@ -60,29 +57,41 @@ namespace Rock.Web.UI.Controls.Communication
         public TransportComponent Transport { get; set; }
 
         /// <summary>
-        /// Sets control values from a communication record.
+        /// Gets or sets the validation group.
         /// </summary>
-        /// <param name="communication">The communication.</param>
-        public override void SetFromCommunication( CommunicationDetails communication )
+        /// <value>
+        /// The validation group.
+        /// </value>
+        public override string ValidationGroup
         {
-            EnsureChildControls();
-            tbTitle.Text = communication.PushTitle;
-            tbMessage.Text = communication.PushMessage;
-            cbSound.Checked = communication.PushSound.IsNotNullOrWhiteSpace();
+            get
+            {
+                EnsureChildControls();
+                return tbMessage.ValidationGroup;
+            }
+            set
+            {
+                EnsureChildControls();
+                mfpMessage.ValidationGroup = value;
+                tbMessage.ValidationGroup = value;
+                tbTitle.ValidationGroup = value;
+            }
         }
+        #endregion
 
+        #region Base Control Methods
         /// <summary>
-        /// Updates the a communication record from control values.
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
         /// </summary>
-        /// <param name="communication">The communication.</param>
-        public override void UpdateCommunication( CommunicationDetails communication )
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
         {
-            EnsureChildControls();
-            communication.PushTitle = tbTitle.Text;
-            communication.PushMessage = tbMessage.Text;
-            communication.PushSound = cbSound.Checked ? "default" : string.Empty;
-        }
+            base.OnInit( e );
 
+            // add the bootstrap-limit so that we can have a countdown of characters when entering push text
+            string script = $"$('#{tbMessage.ClientID}').limit({{maxChars: {1024}, counter:'#{lblCount.ClientID}', normalClass:'badge', warningClass:'badge-warning', overLimitClass: 'badge-danger'}});";
+            ScriptManager.RegisterStartupScript( this, GetType(), $"limit-{ClientID}", script, true );
+        }
         #endregion
 
         #region CompositeControl Methods
@@ -105,14 +114,6 @@ namespace Rock.Web.UI.Controls.Communication
             };
             Controls.Add( tbTitle );
 
-
-            cbSound = new RockCheckBox
-            {
-                ID = string.Format( "cbSound_{0}", this.ID ),
-                Label = "Should make sound?"
-            };
-            Controls.Add( cbSound );
-
             rcwMessage = new RockControlWrapper
             {
                 ID = string.Format( "rcwMessage_{0}", this.ID ),
@@ -132,6 +133,13 @@ namespace Rock.Web.UI.Controls.Communication
             mfpMessage.SelectItem += mfpMergeFields_SelectItem;
             rcwMessage.Controls.Add( mfpMessage );
 
+            lblCount = new Label
+            {
+                CssClass = "badge margin-all-sm pull-right",
+                ID = $"{nameof( lblCount )}_{ID}"
+            };
+            rcwMessage.Controls.Add( lblCount );
+
             tbMessage = new RockTextBox
             {
                 ID = string.Format( "tbTextMessage_{0}", this.ID ),
@@ -149,8 +157,6 @@ namespace Rock.Web.UI.Controls.Communication
 
             };
 
-            iupPushImage.ImageUploaded += iupPushImage_ImageUploaded;
-
             rcwMessage.Controls.Add( iupPushImage );
 
             rbOpenAction = new RockRadioButtonList
@@ -158,7 +164,8 @@ namespace Rock.Web.UI.Controls.Communication
                 ID = $"{nameof( rbOpenAction )}_{ID}",
                 Label = "Open Action",
                 RepeatDirection = RepeatDirection.Horizontal,
-                Help = "Defines the open action for the message."
+                Help = "Defines the open action for the message.",
+                AutoPostBack = true
             };
 
             rbOpenAction.Items.AddRange( new ListItem[] {
@@ -200,23 +207,25 @@ namespace Rock.Web.UI.Controls.Communication
             {
                 ID = $"{nameof( ddlMobileApplications )}_{ID}",
                 Label = "Application",
+                EnableViewState = false
             };
 
             ddlMobileApplications.Items.Add( new ListItem() );
 
-            for(int i = 0; i < mobileSites.Count; i++ )
+            for ( int i = 0; i < mobileSites.Count; i++ )
             {
                 var site = mobileSites[i];
                 ddlMobileApplications.Items.Add( new ListItem( site.Name, site.Id.ToString() ) );
             }
-            
+
             rcwMessage.Controls.Add( ddlMobileApplications );
 
             htmlAdditionalDetails = new HtmlEditor
             {
                 ID = $"{nameof( htmlAdditionalDetails )}_{ID}",
                 Label = "Additional Details",
-                Height = 600
+                Height = 300,
+                EnableViewState = false
             };
 
             rcwMessage.Controls.Add( htmlAdditionalDetails );
@@ -224,7 +233,8 @@ namespace Rock.Web.UI.Controls.Communication
             ppMobilePage = new PagePicker
             {
                 ID = $"{nameof( ppMobilePage )}_{ID}",
-                Label = "Mobile Page"
+                Label = "Mobile Page",
+                EnableViewState = false
             };
 
             rcwMessage.Controls.Add( ppMobilePage );
@@ -233,46 +243,21 @@ namespace Rock.Web.UI.Controls.Communication
             {
                 ID = $"{nameof( kvlQuerystring )}_{ID}",
                 Label = "Mobile Page Query String",
+                EnableViewState = false,
+                KeyPrompt = "Key",
+                ValuePrompt = "Value"
             };
 
             rcwMessage.Controls.Add( kvlQuerystring );
-        }
 
-        private void iupPushImage_ImageUploaded( object sender, ImageUploaderEventArgs e )
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Gets or sets the validation group.
-        /// </summary>
-        /// <value>
-        /// The validation group.
-        /// </value>
-        public override string ValidationGroup
-        {
-            get
+            urlLink = new UrlLinkBox
             {
-                EnsureChildControls();
-                return tbMessage.ValidationGroup;
-            }
-            set
-            {
-                EnsureChildControls();
-                mfpMessage.ValidationGroup = value;
-                tbMessage.ValidationGroup = value;
-                tbTitle.ValidationGroup = value;
-                cbSound.ValidationGroup = value;
-            }
-        }
+                ID = $"{nameof( urlLink )}_{ID}",
+                Label = "URL",
+                EnableViewState = false
+            };
 
-        /// <summary>
-        /// On new communication, initializes controls from sender values
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <exception cref="System.NotImplementedException"></exception>
-        public override void InitializeFromSender( Person sender )
-        {
+            rcwMessage.Controls.Add( urlLink );
         }
 
         /// <summary>
@@ -281,6 +266,25 @@ namespace Rock.Web.UI.Controls.Communication
         /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter" /> object that receives the control content.</param>
         public override void RenderControl( HtmlTextWriter writer )
         {
+            if ( rbOpenAction.SelectedValue.IsNotNullOrWhiteSpace() )
+            {
+                var openAction = ( PushOpenAction ) rbOpenAction.SelectedValue.AsInteger();
+
+                urlLink.Visible = openAction == PushOpenAction.LinkToUrl;
+                kvlQuerystring.Visible = openAction == PushOpenAction.LinkToMobilePage;
+                ppMobilePage.Visible = openAction == PushOpenAction.LinkToMobilePage;
+                ddlMobileApplications.Visible = openAction == PushOpenAction.ShowDetails;
+                htmlAdditionalDetails.Visible = openAction == PushOpenAction.ShowDetails;
+            }
+            else
+            {
+                urlLink.Visible = false;
+                kvlQuerystring.Visible = false;
+                ppMobilePage.Visible = false;
+                ddlMobileApplications.Visible = false;
+                htmlAdditionalDetails.Visible = false;
+            }
+
             // Setting this here because the control clears out this in the OnInit function.
             ppMobilePage.SiteType = SiteType.Mobile;
 
@@ -292,11 +296,6 @@ namespace Rock.Web.UI.Controls.Communication
             tbTitle.RenderControl( writer );
             writer.RenderEndTag();
 
-            writer.AddAttribute( HtmlTextWriterAttribute.Class, "col-md-6" );
-            writer.RenderBeginTag( HtmlTextWriterTag.Div );
-            cbSound.RenderControl( writer );
-            writer.RenderEndTag();
-
             writer.RenderEndTag();
 
             rcwMessage.RenderControl( writer );
@@ -304,7 +303,73 @@ namespace Rock.Web.UI.Controls.Communication
 
         #endregion
 
-        #region Events
+        #region MediumControl Methods
+        /// <summary>
+        /// Sets control values from a communication record.
+        /// </summary>
+        /// <param name="communication">The communication.</param>
+        public override void SetFromCommunication( CommunicationDetails communication )
+        {
+            EnsureChildControls();
+            tbTitle.Text = communication.PushTitle;
+            tbMessage.Text = communication.PushMessage;
+
+            iupPushImage.BinaryFileId = communication.PushImageBinaryFileId;
+            rbOpenAction.SelectedValue = communication.PushOpenAction.ConvertToInt().ToString();
+            htmlAdditionalDetails.Text = communication.PushOpenMessage;
+
+            // TODO: figure this out. communication.PushData
+            var pushData = new PushData();
+            if ( communication.PushData.IsNotNullOrWhiteSpace() )
+            {
+                pushData = Newtonsoft.Json.JsonConvert.DeserializeObject<PushData>( communication.PushData );
+            }
+            ddlMobileApplications.SelectedValue = pushData.MobileApplicationId.ToStringSafe();
+            ppMobilePage.SetValue( pushData.MobilePageId );
+            urlLink.Text = pushData.Url;
+            kvlQuerystring.Value = null;
+            if ( pushData.MobilePageQueryString != null )
+            {
+                kvlQuerystring.Value = pushData.MobilePageQueryString.Select( a => string.Format( "{0}^{1}", a.Key, a.Value ) ).ToList().AsDelimited( "|" );
+            }
+
+            
+        }
+
+        /// <summary>
+        /// Updates the a communication record from control values.
+        /// </summary>
+        /// <param name="communication">The communication.</param>
+        public override void UpdateCommunication( CommunicationDetails communication )
+        {
+            EnsureChildControls();
+            communication.PushTitle = tbTitle.Text;
+            communication.PushMessage = tbMessage.Text;
+            communication.PushImageBinaryFileId = iupPushImage.BinaryFileId;
+            communication.PushOpenAction = ( PushOpenAction ) rbOpenAction.SelectedValue.AsIntegerOrNull();
+            communication.PushOpenMessage = htmlAdditionalDetails.Text;
+
+            var pushData = new PushData
+            {
+                MobileApplicationId = ddlMobileApplications.SelectedValue.AsIntegerOrNull(),
+                MobilePageQueryString = kvlQuerystring.Value.AsDictionaryOrNull(),
+                MobilePageId = ppMobilePage.SelectedValue.AsIntegerOrNull(),
+                Url = urlLink.Text
+            };
+            communication.PushData = pushData.ToJson();
+        }
+
+        /// <summary>
+        /// On new communication, initializes controls from sender values
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override void InitializeFromSender( Person sender )
+        {
+        }
+        #endregion
+
+        #region Control Events
 
         void mfpMergeFields_SelectItem( object sender, EventArgs e )
         {
@@ -312,7 +377,6 @@ namespace Rock.Web.UI.Controls.Communication
             tbMessage.Text += mfpMessage.SelectedMergeField;
             mfpMessage.SetValue( string.Empty );
         }
-
         #endregion
     }
 }
